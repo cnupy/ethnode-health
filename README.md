3089c600c8b5a695d56a43450ecac96bfbe32eb5
ivan <ivan.petkov@gmail.com>
2024-02-25 10:48:17 +0000

    projoect init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Overview
+
+Docker Compose for healt monitoring of Ethereum node
+
+`cp .env.example .env`, then `nano .env` and adjust values for CL, EL. 
+
+`nano network.yaml` to adjust ext networks
+
+To start the container 
+
+`docker compose build && docker compose up -d`
+
+Inspired by https://github.com/CryptoManufaktur-io
---
+++ webhook-scripts/check-ccsync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+MIN_PEERS=10
+SYNC=$(curl -s -m2 -N "${CC_ADDRESS}/eth/v1/node/syncing")
+echo "${SYNC}" | grep -q "data"
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+SYNCING=$(echo "${SYNC}" | jq .data.is_syncing)
+OPTIMISTIC=$(echo "${SYNC}" | jq .data.is_optimistic)
+EL_OFFLINE=$(echo "${SYNC}" | jq .data.el_offline)
+PEERS=$(curl -s -m2 -N "${CC_ADDRESS}/eth/v1/node/peer_count")
+echo "${PEERS}" | grep -q "data"
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+PEERS=$(echo "${PEERS}" | jq -r .data.connected)
+if [ "${SYNCING}" = "false" -a "${OPTIMISTIC}" = "false" -a "${EL_OFFLINE}" = "false" -a "${PEERS}" -ge "$MIN_PEERS" ]; then
+  echo "OK"
+  exit 0
+else
+  exit 1
+fi
